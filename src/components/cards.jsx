9fdeb07953effc7f2cdab589a0944e47d58e0698
Hudu <ahmedhudu21@gmail.com>
2023-11-16 13:26:55 +0100

    cards sections

---
+++ src/components/cards.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <div>
+      <div className="card w-96 bg-primary text-primary-content">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>category description</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
