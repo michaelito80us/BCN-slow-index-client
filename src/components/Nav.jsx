5a7836eb7ef94948c38d5c22a82898cb828a6570
Pedro Dias <pedromndias@gmail.com>
2023-11-16 13:12:40 +0100

    added config daisyui

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import logo1 from "../assets/logo-1.png"
+
+const Nav = () => {
+  return (
+    <div className="navbar bg-base-100">
+        <img src={logo1} alt="logo" />
+    </div>
+  )
+}
+
+export default Nav
