4033241c261f947e841a080a0a248cd5826ccf27
Hudu <ahmedhudu21@gmail.com>
2023-11-16 17:35:26 +0100

    button, card and button added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./components/Button";
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
-    colors: {
-      primary: "#57BD36",
-      secondary: "#394648",
-      accent: "#CBAC88",
-    },
     extend: {
-      color: {
+      colors: {
         primary: "#57BD36",
         secondary: "#394648",
         accent: "#CBAC88",
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
