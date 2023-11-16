58f5a2a32b3f3d1f1ca9ea5b574892e6745f0163
Pedro Dias <pedromndias@gmail.com>
2023-11-16 13:25:35 +0100

    Added Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
+import Nav from "./components/Nav";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-8 text-center">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img
-            src={viteLogo}
-            className="h-24 p-6 hover:drop-shadow-lg"
-            alt="Vite logo"
-          />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img
-            src={reactLogo}
-            className="h-24 p-6 hover:drop-shadow-2xl"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1 className="bg-secondary text-5xl text-primary">Vite + React</h1>
-      <div className="p-8">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-gray-500">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import logo1 from "../assets/logo-1.png"
+
+const Nav = () => {
+  return (
+    <div className="navbar bg-base-100 bg-accent">
+        <img src={logo1} alt="logo" />
+    </div>
+  )
+}
+
+export default Nav
