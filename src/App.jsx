373db1a2e1def988d4dbaaa6d7eb515b80f18c43
Pedro Dias <pedromndias@gmail.com>
2023-11-16 14:01:42 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 
 function App() {
   
 
   return (
-    <div>
+    <div className="flex flex-col h-screen justify-between">
       <Nav />
+      <div className="items-center w-100">
+        <h1>Barcelona Slow Index</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, sint eaque vel eligendi ea molestias incidunt dolores, voluptas tenetur quam quod quae aspernatur? Ipsa animi suscipit dolore praesentium maxime quos!</h2>
+      </div>
+      <Footer />
     </div>
   );
 }
